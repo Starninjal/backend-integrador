e09463ee2e9781a4daa0791f1ac2cd6ab86a7be5
Starninjal <BR17johnn@gmail.com>
2024-05-03 10:22:30 -0300

    main update

--- integrador/src/main/java/com/wmw/backend/integrador/IntegradorApplication.java
+++ integrador/src/main/java/com/wmw/backend/integrador/IntegradorApplication.java
@@ -2,10 +2,26 @@ package com.wmw.backend.integrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@CrossOrigin("*")
 @SpringBootApplication
 public class IntegradorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IntegradorApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer
+	webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+
+			}
+		};
 	}
 }
