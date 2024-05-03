a0b0e90276d78be35266aae5dc1c6cfd9c1f31b0
Starninjal <BR17johnn@gmail.com>
2024-05-03 10:35:02 -0300

    updating ClienteService

--- integrador/src/main/java/com/wmw/backend/integrador/service/ClienteService.java
+++ integrador/src/main/java/com/wmw/backend/integrador/service/ClienteService.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.wmw.backend.integrador.dto.ClienteDTO;
+import com.wmw.backend.integrador.dto.SincronizacaoDataDTO;
 import com.wmw.backend.integrador.exception.ClienteException;
 import com.wmw.backend.integrador.model.cliente.Cliente;
 import com.wmw.backend.integrador.model.cliente.TipoPessoa;
@@ -16,14 +18,15 @@ import com.wmw.backend.integrador.utils.UtilCliente;
 @Service
 public class ClienteService {
 
-
-
 	@Autowired
 	private ClienteRepository clienteRepository;
 
 	public Cliente cadastrarCliente(ClienteDTO clienteDTO) {
 		validarCliente(clienteDTO);
-		Cliente cliente = new Cliente();
+		if(validarClienteExistente(clienteDTO)) {
+		throw new DataIntegrityViolationException("Documento existente!");
+	}
+ 		Cliente cliente = new Cliente();
 		BeanUtils.copyProperties(clienteDTO, cliente);
 		return clienteRepository.save(cliente);
 	}
@@ -39,20 +42,28 @@ public class ClienteService {
 
 	public Cliente alterarCliente(ClienteDTO clienteDTO) {
 		Cliente clienteExistente = buscarClientePor(clienteDTO.id());
-		if(!(clienteDTO.email() == null)) { 
+		if (!(clienteDTO.email() == null)) {
 			clienteExistente.setEmail(clienteDTO.email());
 			clienteExistente.setTelefone(clienteDTO.telefone());
 			return clienteRepository.save(clienteExistente);
 		}
-		
+
 		return clienteExistente;
 	}
 
 	public void excluirCliente(Long id) {
+		Cliente cliente = listarClientes().stream()
+				.filter(c -> c.getId() == id)
+				.findFirst()
+				.orElseThrow(() -> new NullPointerException("Cliente não encontrado"));
+		UtilCliente.addClienteDeletado(cliente);
 		clienteRepository.deleteById(id);
 	}
 
 	public void validarCliente(ClienteDTO clienteDTO) {
+		UtilCliente.validarTelefone(clienteDTO);
+		UtilCliente.validarEmail(clienteDTO);
+		
 		if (clienteDTO.tipoPessoa() == TipoPessoa.FISICA) {
 			if (!validarCpf(clienteDTO.documento())) {
 				throw new ClienteException("CPF inválido, tente novamente!");
@@ -60,6 +71,49 @@ public class ClienteService {
 		} else if (clienteDTO.tipoPessoa() == TipoPessoa.JURIDICA) {
 			if (!validarCnpj(clienteDTO.documento())) {
 				throw new ClienteException("CNPJ inválido, tente novamente!");
+			}
+		}
+	}
+
+
+	public void sincronizarClientesMobile(SincronizacaoDataDTO sincronizacaoData) {
+		List<ClienteDTO> clientesDTO = sincronizacaoData.getClientesDTO();
+		List<ClienteDTO> clientesRemovidosDTO = sincronizacaoData.getClientesDeletadosDTO();
+		if (clientesDTO != null) {
+			for (ClienteDTO cliDTO : clientesDTO) {
+				if (cliDTO.tipoPessoa() == TipoPessoa.FISICA || cliDTO.tipoPessoa() == TipoPessoa.JURIDICA){
+					boolean clienteExiste = validarClienteExistente(cliDTO);
+
+					if (!clienteExiste) {
+						cadastrarCliente(cliDTO);
+					} else {
+						Cliente clienteOrigem = clienteRepository.findByDocumento(cliDTO.documento());
+						cliDTO = cliDTO.withId(clienteOrigem.getId());
+						alterarCliente(cliDTO);
+					}
+				} else {
+					throw new IllegalArgumentException("Houve um erro na sincronização");
+				}	
+			}
+			
+			if (clientesRemovidosDTO.size() > 0) {
+				removerClientesApi(clientesRemovidosDTO);
+			}
+		} else {
+			excluirTodosClientes();
+		}
+	}
+	private void excluirTodosClientes() {
+		clienteRepository.deleteAll();
+	}
+
+	private void removerClientesApi(List<ClienteDTO> clientesRemovidos) {
+		List<Cliente> clientesAtuais = listarClientes();
+		for(ClienteDTO cliDTO: clientesRemovidos) {
+			Cliente clienteOrigem = clienteRepository.findByDocumento(cliDTO.documento());
+			cliDTO.withId(clienteOrigem.getId());
+			if(validarClienteExistente(cliDTO)) {
+				clienteRepository.deleteById(clienteOrigem.getId());
 			}
 		}
 	}
@@ -82,7 +136,9 @@ public class ClienteService {
 			return false;
 		}
 
-		cnpj = cnpj.replaceAll("[^0-9]", "");
+		String regexRemoverLetras = "[^0-9]";
+
+		cnpj = cnpj.replaceAll(regexRemoverLetras, "");
 
 		int digitoVerificador1 = UtilCliente.calcularDigitoCNPJ(cnpj, 12);
 		int digitoVerificador2 = UtilCliente.calcularDigitoCNPJ(cnpj, 13);
@@ -91,13 +147,9 @@ public class ClienteService {
 				&& Integer.parseInt(String.valueOf(cnpj.charAt(13))) == digitoVerificador2;
 
 	}
-
-
-
-
 	
-	
-	
-	
-
+	private boolean validarClienteExistente(ClienteDTO clienteDTO) {
+		List<Cliente> clientes = listarClientes();
+		return clientes.stream().filter(c -> c != null).anyMatch(c -> c.getDocumento().equals(clienteDTO.documento())); 
+	}
 }
