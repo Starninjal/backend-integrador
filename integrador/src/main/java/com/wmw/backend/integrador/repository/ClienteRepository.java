333fe185bbc60a5db67ebe7f01110fb3f41371ef
Starninjal <BR17johnn@gmail.com>
2024-05-03 10:34:30 -0300

    updating ClienteRepository

--- integrador/src/main/java/com/wmw/backend/integrador/repository/ClienteRepository.java
+++ integrador/src/main/java/com/wmw/backend/integrador/repository/ClienteRepository.java
@@ -1,9 +1,8 @@
 package com.wmw.backend.integrador.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.wmw.backend.integrador.model.cliente.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-	
+	Cliente findByDocumento(String documento);
 }
